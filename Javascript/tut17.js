d47109baf184141b414200d2a4d5853804949aa1
Sanskarjain04 <116282600+Sanskarjain04@users.noreply.github.com>
2023-09-26 23:43:08 +0530

    created tut17

---
+++ Javascript/tut17.js
@@ -0,0 +1,8 @@
+//Array Methods
+let num = [13,45,75,35,34]
+let b=num.toString()// b is now a string
+console.log(typeof b)
+let c=num.join(" and ")
+console.log(c)
+let r=num.pop()
+console.log(r)
